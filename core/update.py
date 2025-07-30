d6bf8055134a551eb04504a278bf950349995c45
HG <differentg021@gmail.com>
2025-07-30 11:24:00 +0900

    [refactor] add default env value

--- core/update.py
+++ core/update.py
@@ -3,8 +3,8 @@ from datetime import datetime, timezone, timedelta
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 
 INFLUXDB_URL = os.getenv("INFLUXDB_URL", "http://influxdb:8086")
-INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN", "")
-INFLUXDB_ORG = os.getenv("INFLUXDB_ORG", "")
+INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN", "watchcat-token")
+INFLUXDB_ORG = os.getenv("INFLUXDB_ORG", "watchcat_org")
 INFLUXDB_BUCKET = os.getenv("INFLUXDB_BUCKET", "watchcat")
 
 client = InfluxDBClient(
